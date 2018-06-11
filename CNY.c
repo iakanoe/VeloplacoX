e71231b4fb600dc58fbf222672f265b584568804
iakanoe <iakanoe1056@gmail.com>
2018-06-10 23:32:35 -0300

    second commit

--- CNY.c
+++ CNY.c
@@ -7,11 +7,11 @@ unsigned int data[] = {0, 0, 0, 0, 0};
 
 void initCNY(void){
     ADCON1 = 0x0A;
-    //ADCON2 = 0x10xxxxyyy;
+    ADCON2 = 0xBE;
     PIR1bits.ADIF = 0;
     PIE1bits.ADIE = 1;
-    ADCON0bits.ADON = 1;
     ADCON0bits.CHS = channels[actualChannel];
+    ADCON0bits.ADON = 1;
     ADCON0bits.GO = 1;
 }
 
